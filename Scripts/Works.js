ca43f2ca8ab3e8fea916040f60f84560f2292e4f
joy111joy <150044778+joy111joy@users.noreply.github.com>
2024-04-11 22:20:27 -0230

    Delete Scripts directory

--- Scripts/Works.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var worksBoxes = document.getElementsByClassName('WorksBox');
-    for (var i = 0; i < worksBoxes.length; i++) {
-        worksBoxes[i].addEventListener('click', function() {
-            this.classList.toggle('flipped');
-        });
-    }
-}
